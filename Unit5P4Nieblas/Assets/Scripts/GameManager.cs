b886a01b977c24c4086018075192e0e68c311206
frosh-cloud <757085@student.dusd.net>
2025-05-20 11:45:43 -0700

    Worked on my unit 5 project
    I added animations to my crates.

--- Unit5P4Nieblas/Assets/Scripts/GameManager.cs
+++ Unit5P4Nieblas/Assets/Scripts/GameManager.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    private int score;
     private float spawnRate = 1.0f;
    
 
@@ -13,6 +17,8 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -28,7 +34,19 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            
         }
         
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
     }
 }
